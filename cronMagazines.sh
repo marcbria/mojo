1489c3ea866bfb59f975e7446b587a26c8ee2fe4
Julia Mestieri (Projecte Ictineo SCCL) <juli@projecteictineo.com>
2014-01-14 14:46:53 +0100

    configuration set to external file config.mojo

--- cronMagazines.sh
+++ cronMagazines.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-# Recreated on: 20140114-4128
+# Recreated on: 20140114-4600
 
 # More info: http://pkp.sfu.ca/ojs/docs/userguide/2.3.1/systemAdministrationGeneralConfig.html
+php /home/ojs/htdocs/ojs-testc2/tools/runScheduledTasks.php
+php /home/ojs/htdocs/ojs-testc17/tools/runScheduledTasks.php
 php /home/ojs/htdocs/ojs-test998/tools/runScheduledTasks.php
+php /home/ojs/htdocs/ojs-testc1/tools/runScheduledTasks.php
 php /home/ojs/htdocs/ojs-test99/tools/runScheduledTasks.php
 php /home/ojs/htdocs/ojs-test999/tools/runScheduledTasks.php
+php /home/ojs/htdocs/ojs-testc/tools/runScheduledTasks.php
+php /home/ojs/htdocs/ojs-testc12/tools/runScheduledTasks.php
+php /home/ojs/htdocs/ojs-testcc/tools/runScheduledTasks.php
--- scripts/mojo.sh
+++ scripts/mojo.sh
@@ -75,47 +75,47 @@
 #      REVISION:  0.28
 #===============================================================================
 
-# SCRIPT CONFIGURATION CONSTANTS: ==============================================
-# ToDo: Move to an external file? Include pwds?
-
+# SCRIPT CONFIGURATION CONSTANTS TAKEN FROM config.mojo=========================
+# Taken variables are:
 # Path of the service root (P.e: /home/ojs)
-PATHBASE="/home/ojs"
-
+# PATHBASE="/home/ojs"
+#
 # Path of the folder with the current version (P.e: $URLBASE/source/versions/current)
-PATHMOTOR="$PATHBASE/source/versions/current"
-
+# PATHMOTOR="$PATHBASE/source/versions/current"
+#
 # Path of the web root (P.e: $PATHBASE/htdocs)
-PATHWEB="$PATHBASE/htdocs"
-
+# PATHWEB="$PATHBASE/htdocs"
+#
 # Path of the storage folder (P.e: $PATHBASE/webdata)
-PATHDATA="$PATHBASE/webdata"
-
+# PATHDATA="$PATHBASE/webdata"
+#
 # Path of the backup folder (P.e: $PATHBASE/backup)
-PATHBACKUP="$PATHBASE/backup"
-
+# PATHBACKUP="$PATHBASE/backup"
+#
 # Base URL for the service (P.e: http://revistes.uab.cat)
-URLBASE="http://revistes.uab.cat"
-# URLBASE="http://magazine.localhost.net"
-
+# URLBASE="http://revistes.uab.cat"
 # Path of the temporal folder (Pe: /tmp/mojo)
-PATHTMP="/tmp/mojo"
-
+# PATHTMP="/tmp/mojo"
+#
 # DB dump of the OJS model
-DBDUMP="dumpBaseNew.sql"
-
-# Build your own aternative bases:
-# PATHMOTOR="$PATHBASE/source/versions/ojs-2.4-redi-git-alec"
-# PATHMOTOR="$PATHBASE/source/versions/fastrad"
-
-# Working fine with 2.4:
-# PATHMOTOR="$PATHBASE/source/versions/trad24"
-
-# Refer your own alternative DB dumps:
-# DBDUMP="baseFinal.sql"
-# DBDUMP="baseFormacio.sql"
-
+# DBDUMP="dumpBaseNew.sql"
+#
 # Even more verbose
-DEBUG=false
+# DEBUG=false
+
+# LOAD CONFIGURATION FILE
+#
+# Get current path
+SCRIPT="`readlink -e $0`"
+SCRIPTPATH="`dirname $SCRIPT`"
+# Load file if exists
+if [ -f $SCRIPTPATH/config.mojo ] ; then
+  . $SCRIPTPATH/config.mojo
+else
+  echo "ERROR: Configuration file config.mojo doesn't exists"
+  exit 
+fi
+
 
 #===============================================================================
 
